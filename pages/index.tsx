32ae0f3951ddda51e5832757d0ba2555d400bfe5
hamumeron <nekosuke.1012@outlook.jp>
2025-06-28 09:05:14 +0900

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-// pages/index.tsx
-export default function Desktop() {
-  return (
-    <div className="h-screen w-screen bg-gradient-to-br from-slate-900 to-gray-800 text-white">
-      {/* ここにデスクトップアイコン、スタートバー、ウィンドウが配置される */}
-    </div>
-  );
-}
